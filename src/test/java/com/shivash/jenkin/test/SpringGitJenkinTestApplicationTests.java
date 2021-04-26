0c4f96bc4abb5b27a0ace2991ec8659105d21f90
Shivansh123321 <shivansh123321@gmail.com>
2021-04-26 12:37:12 +0530

    8th commit

--- src/test/java/com/shivash/jenkin/test/SpringGitJenkinTestApplicationTests.java
+++ src/test/java/com/shivash/jenkin/test/SpringGitJenkinTestApplicationTests.java
@@ -13,8 +13,6 @@ class SpringGitJenkinTestApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("test case executing....");
-		logger.info("test case executing second time....");
-		logger.info("test case executing third time....");
 		assertEquals(true,true);
 	}
 
